58081389eab1b161034ea26ab143deed4cc8bf2e
Erica Perry <erica.perry@woodlanddirect.com>
2020-02-24 19:52:01 -0500

    add: scripts for build

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
+    { pathPrefix: '/gatsby-portfolio' },
     {
       resolve: `gatsby-plugin-scroll-reveal`,
       options: {
